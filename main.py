4cf8918068e99e90c9acc13884d4f5c5d0db21ab
Ali Mostafa <amostafa1@cps.edu>
2025-01-14 10:56:25 -0600

    add testing and small refactoring

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ import pandas as pd
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt
 
-data = pd.read_csv("obesity_dataset.csv")
-data["NObeyesdad"].replace(
-    ["Insufficient_Weight",
+weight_classes = [
+    "Insufficient_Weight",
      "Normal_Weight",
      "Overweight_Level_I",
      "Overweight_Level_II",
      "Obesity_Type_I",
      "Obesity_Type_II",
-     "Obesity_Type_III"],
+     "Obesity_Type_III"
+]
+
+data = pd.read_csv("obesity_dataset.csv")
+data["NObeyesdad"].replace(
+    weight_classes,
      [0,1,2,3,4,5,6], inplace=True)
 
 data["Gender"].replace(["Male", "Female"], [0, 1], inplace=True)
@@ -34,30 +37,26 @@ model = linear_model.LogisticRegression().fit(x_train, y_train)
 
 print("Accuracy:", model.score(x_test, y_test))
 
-# test example
-# example = [[0, 45, 1, 0, 1, 3]]
-# scaler.fit(example)
-# example = scaler.transform(example)
+predict = model.predict(x_test)
 
-# print(model.predict(example))
+avg_diff = 0
 
-# fig, graph = plt.subplots(4)
+def mapnames(x):
+    return weight_classes[x]
 
-# graph[0].scatter(data["family_history_with_overweight"], y)
-# graph[0].set_xlabel("Family History With Overweight")
-# graph[0].set_ylabel("Obesity Level")
+for index in range(len(x_test)):
+    actual = y_test[index][0]
+    predicted_y = predict[index]
+    x_coord = x_test[index]
 
-# graph[1].scatter(data["FAF"], y)
-# graph[1].set_xlabel("Regularity of Physical Activity")
-# graph[1].set_ylabel("Obesity Level")
+    actual_name = mapnames(actual)
+    predicted_y_name = mapnames(predicted_y)
 
-# graph[2].scatter(data["SCC"], y)
-# graph[2].set_xlabel("Monitoring Caloric Intake")
-# graph[2].set_ylabel("Obesity Level")
+    print(f"Actual: {actual_name} Predicted: {predicted_y_name}")
+    if actual == predicted_y:
+        print("Correct!")
+    else:
+        avg_diff += abs(predicted_y - actual)
+        print("Incorrect. Difference: " + str(abs(predicted_y - actual)))
 
-# graph[3].scatter(data["CAEC"], y)
-# graph[3].set_xlabel("Eating Between Meals")
-# graph[3].set_ylabel("Obesity Level")
-
-# plt.tight_layout()
-# plt.show()
+print("average difference: " + str(avg_diff/len(x_test)))
